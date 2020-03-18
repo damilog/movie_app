8ce153c21f08eebec5da5a6ee3b68d4d734d9624
damilog <dami.k.dev@gmail.com>
2020-03-18 23:54:47 +0900

    add : Make Components with PropTypes
    - Food 컴포넌트 생성 후 name, picture, rating props 작성
    - Food PropTypes 작성

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+const foodIlike = [
+  {
+    name : "kimchi",
+    picture : "https://craftlog.com/m/i/6171598=s1280=h960.webp",
+    rating : 1
+  },
+  {
+    name : "Ramen",
+    picture : "https://file.mk.co.kr/meet/neds/2017/09/image_readtop_2017_587233_15042337473013492.jpg",
+    rating : 2
+ 
+  }
+];
+
+function Food({name, picture, rating}){
+  return(
+  <div>
+      <h2> I like {name} </h2>
+      <h4> {rating} /5.0</h4>
+      <img src = {picture} alt={name} />
+  </div> 
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.string.isRequired                        
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+     <div>
+        {foodIlike.map(dish => (
+          <Food key = {dish.id} 
+          name={dish.name} 
+          picture={dish.picture} 
+          rating = {dish.rating}/>
+        ))}
     </div>
-  );
+  );  
 }
 
 export default App;
