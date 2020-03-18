37daecb4f041dc51cac068a8d588a35b60b20309
damilog <dami.k.dev@gmail.com>
2020-03-19 00:54:04 +0900

    ADD : add class component for using 'state'
    - Remove food component
    - Add add, minus button
    - Add add, minus function and test (console.log)
    - Use add, minus function in button script

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 
-const foodIlike = [
-  {
-    name : "kimchi",
-    picture : "https://craftlog.com/m/i/6171598=s1280=h960.webp",
-    rating : 1
-  },
-  {
-    name : "Ramen",
-    picture : "https://file.mk.co.kr/meet/neds/2017/09/image_readtop_2017_587233_15042337473013492.jpg",
-    rating : 2
- 
+class App extends React.Component {
+  state = {
+    count : 0
+  };
+  add = () => {
+    console.log("add");
+
+  };
+  minus = () => {
+    console.log("minus");
+
+  };
+
+  render(){
+    return (
+    <div>
+       <h1>The number is {this.state.count}</h1>
+       <button onClick = {this.add}>Add</button>
+       <button onClick = {this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-function Food({name, picture, rating}){
-  return(
-  <div>
-      <h2> I like {name} </h2>
-      <h4> {rating} /5.0</h4>
-      <img src = {picture} alt={name} />
-  </div> 
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.string.isRequired                        
-};
-
-function App() {
-  return(
-     <div>
-        {foodIlike.map(dish => (
-          <Food key = {dish.id} 
-          name={dish.name} 
-          picture={dish.picture} 
-          rating = {dish.rating}/>
-        ))}
-    </div>
-  );  
 }
 
 export default App;
