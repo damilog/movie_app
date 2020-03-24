ac435ff3b2718fb2e338caeaa3ab61d5d0150da9
damilog <dami.k.dev@gmail.com>
2020-03-24 23:17:23 +0900

    Revert "Feat :  Add JSX for UI design"
    This reverts commit 32fdffad996bac28e619ce3cad7443fbb5614b0e.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
-import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -9,7 +8,7 @@ class App extends React.Component {
     movies:[]
   };
   getMovies = async() => {
-    const { 
+    const {
       data: {
       data: { movies }
        }
@@ -23,13 +22,10 @@ class App extends React.Component {
   render() {
     const { isLoading, movies} = this.state;
     return (
-      <section className="container">
-        {isLoading ?( <div className ="loader">
-          <span className="loader__text">Loading...</span>
-        </div>
-        ): (
-          <div className ="movies">
-            {movies.map(movie => (
+      <div>
+        {isLoading
+        ? "Loading..." 
+        : movies.map(movie => (
            <Movie 
             key={movie.id}
             id ={movie.id}
@@ -37,12 +33,9 @@ class App extends React.Component {
             title={movie.title} 
             summary={movie.summary} 
             poster={movie.medium_cover_image}
-            genres={movie.genres}
               />
-            ))}
-          </div>
-          )}
-        </section>
+              ))}
+        </div>
    );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,25 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-
-//movie component
-function Movie({id, year, title, summary, poster,genres}){
-    return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title}/>
-      <div className="movie__data">
-        <h3 className="moive__title">{title}</h3>
-        <h5 className="moive__year">{year}</h5>
-        <ul className="movie__genres">
-            {genres.map((genre, index) => (
-           <li key={index} className="genres__genre">
-               {genre}</li> 
-           ))}
-        </ul>
-        <p className="moive__summary">{summary}</p>
-      </div>
-    </div>
-    );
+function Movie({id, year, title, summary, poster}){
+    return <h4>{title}</h4>;
 }
 
 Movie.propTypes = {
@@ -27,8 +10,7 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+    poster: PropTypes.string.isRequired
 };
 
 export default Movie;
